741102484a952c55d516e401174211020e262537
Joel Binnquist <joel.binnquist@gmail.com>
2016-11-05 17:24:35 +0100

    WIP...

--- typescript-language/src/use3ppGlobal.ts
+++
@@ -1,7 +0,0 @@
-import {expect} from "chai";
-
-describe("@types för lib som stödjer global defintiion", () => {
-  it("Lodash kan även refereras utan explicit import eftersom den även är deklararerad som global", ()=>{
-    expect(_.map([1,2,3], x => x* 2)).to.eql([2,4,6]);
-  });
-});
