de23487b798d018735b6a8308f245cd1511ab1f0
Joel Binnquist <joel.binnquist@gmail.com>
2016-11-06 14:18:39 +0100

    WIP...

---
+++ typescript-language/src/playground.spec.ts
@@ -0,0 +1,11 @@
+import {expect} from "chai";
+
+describe("Lekplats", () => {
+  it("Se till att mocha och chai snurrar", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("Hacka loss", () => {
+    // Här kan du skriva din kod...
+  });
+});
